e0e99ed1a973103390dda1e7bce27f7e9849fec8
PrinceStiles <princekarpture@gmail.com>
2023-11-27 13:18:24 +0000

    app

--- I.POL-frontend/app/index.js
+++ I.POL-frontend/app/index.js
@@ -1,11 +1,95 @@
 import { Stack, useRouter } from "expo-router";
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { COLORS, icons, images, SIZES, FONT } from "../constants";
+import { ProfileHeaderBtn } from "../components";
+import start from "../assets/start.jpeg";
 
 const Home = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.White,
+        padding: SIZES.medium,
+      }}
+    >
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.White,
+          },
+          headerShadowVisible: false,
+          headerTitle: "",
+          headerLeft: () => (
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <ProfileHeaderBtn />
+              <Text
+                style={{
+                  fontFamily: FONT.bold,
+                  color: COLORS.primary,
+                  fontSize: SIZES.small,
+                }}
+              >
+                I.POL
+              </Text>
+            </View>
+          ),
+          headerRight: () => (
+            <ProfileHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+        }}
+      />
+
+      <View style={{ width: "100%", display: "flex", flexDirection: "column" }}>
+        <Text
+          style={{
+            color: "#212121",
+            textAlign: "left",
+            fontFamily: FONT.bold,
+            fontSize: SIZES.xLarge,
+            fontWeight: 700,
+            paddingTop: 60,
+            paddingBottom: 30,
+            lineHeight: "normal",
+          }}
+        >
+          Make a Quick Quote
+        </Text>
+        <TouchableOpacity
+          onPress={() => {
+            router.push(`/signin`);
+          }}
+          style={{
+            width: 150,
+            height: 30,
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: SIZES.small,
+            backgroundColor: COLORS.primary,
+          }}
+        >
+          <Text style={{ color: COLORS.lightWhite }}>Sign In</Text>
+        </TouchableOpacity>
+        <View style={{ display: "flex", paddingTop: 80, paddingLeft: 100 }}>
+          <Image source={start} style={{ width: 200, height: 200 }} />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
