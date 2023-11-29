3c3df60dde341dea127baff323ab8231a1f82afb
PrinceStiles <princekarpture@gmail.com>
2023-11-29 12:15:17 +0000

    firebase-config

---
+++ I.POL-frontend/config/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "@react-native-firebase/app";
+import { getAnalytics } from "@react-native-firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAWFJF0Za8-IdvynxaUjiE2PgYqwQIfQLg",
+  authDomain: "ipol-app.firebaseapp.com",
+  projectId: "ipol-app",
+  storageBucket: "ipol-app.appspot.com",
+  messagingSenderId: "165832932863",
+  appId: "1:165832932863:web:bf8333895d3302d45fbbca",
+  measurementId: "G-Z1VG9W3QDT",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
