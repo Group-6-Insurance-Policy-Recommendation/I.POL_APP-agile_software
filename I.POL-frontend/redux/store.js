1d6472fad4e1cc4dd6df634e2fa404ebcac73d6a
PrinceStiles <princekarpture@gmail.com>
2023-12-03 09:37:29 +0000

    redux state

--- I.POL-frontend/redux/store.js
+++ I.POL-frontend/redux/store.js
@@ -1,8 +1,12 @@
 // store.js
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/authReducer';
+// import { applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/authReducer";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+});
 
 export default store;
