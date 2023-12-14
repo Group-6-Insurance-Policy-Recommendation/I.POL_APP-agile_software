fed8fa1cad2ddce49e8d6c405457c3d4a78b1dfd
PrinceStiles <princekarpture@gmail.com>
2023-12-14 16:34:12 +0000

    dummy data

---
+++ I.POL-frontend/data/companyPolicy.js
@@ -0,0 +1,177 @@
+// policies.js
+
+const companyPolicies = [
+  // ... (previous policies)
+
+  {
+    id: "12",
+    logo: "https://via.placeholder.com/150/33A5FF/FFFFFF?text=Company+G",
+    company: "TravelGuard Insurance",
+    name: "Explorer Travel Plan",
+    coverage: "Trip Cancellation, Emergency Medical, Lost Baggage",
+    premium: "$120 per trip",
+    description:
+      "Tailored for avid travelers, covering trip cancellations, emergency medical expenses, and lost baggage protection.",
+    additionalBenefits: [
+      "24/7 Emergency Assistance",
+      "Coverage for Adventure Activities",
+      "Travel Delay Reimbursement",
+    ],
+    exclusions: [
+      "Pre-existing Medical Conditions",
+      "Extreme Sports not covered",
+    ],
+  },
+
+  {
+    id: "13",
+    logo: "https://via.placeholder.com/150/33A5FF/FFFFFF?text=Company+G",
+    company: "SecureLife Insurance",
+    name: "Family Shield Plan",
+    coverage: "Life Insurance, Critical Illness, Children’s Education Fund",
+    premium: "$50 per month",
+    description:
+      "Comprehensive family protection plan, including life insurance, critical illness coverage, and a fund for children’s education.",
+    additionalBenefits: [
+      "Accidental Death Benefit",
+      "Hospital Cash Benefit",
+      "Flexible Premium Payment Options",
+    ],
+    exclusions: [
+      "Suicide within the first year of coverage",
+      "Pre-existing Critical Illnesses",
+    ],
+  },
+
+  {
+    id: "14",
+    logo: "https://via.placeholder.com/150/33A5FF/FFFFFF?text=Company+G",
+    company: "SafeDrive Insurance",
+    name: "AutoGuard Plus",
+    coverage: "Auto Liability, Collision, Comprehensive",
+    premium: "$800 per year",
+    description:
+      "Tailored auto insurance offering liability coverage, collision protection, and comprehensive coverage against theft and natural disasters.",
+    additionalBenefits: [
+      "Roadside Assistance",
+      "New Car Replacement",
+      "Rental Car Reimbursement",
+    ],
+    exclusions: ["Driving under the influence (DUI)", "Intentional Damage"],
+  },
+
+  {
+    id: "15",
+    logo: "https://via.placeholder.com/150/33A5FF/FFFFFF?text=Company+G",
+    company: "HealthWell Assurance",
+    name: "WellnessGuard Health Plan",
+    coverage: "Health Insurance, Preventive Care, Prescription Medications",
+    premium: "$120 per month",
+    description:
+      "Comprehensive health plan covering medical expenses, preventive care, and prescription medications for individuals and families.",
+    additionalBenefits: [
+      "Dental and Vision Coverage",
+      "Health Savings Account (HSA)",
+      "Telemedicine Services",
+    ],
+    exclusions: ["Cosmetic Surgeries", "Experimental Treatments"],
+  },
+
+  {
+    id: "16",
+    logo: "https://via.placeholder.com/150/33A5FF/FFFFFF?text=Company+G",
+    company: "TravelSafe Insurance",
+    name: "Explorer Plus Travel Policy",
+    coverage: "Trip Cancellation, Emergency Medical Expenses, Lost Luggage",
+    premium: "$150 per trip",
+    description:
+      "Tailored travel insurance providing coverage for trip cancellations, emergency medical expenses, and lost or delayed luggage during travels.",
+    additionalBenefits: [
+      "24/7 Emergency Assistance",
+      "Travel Delay Reimbursement",
+      "Adventure Sports Coverage",
+    ],
+    exclusions: [
+      "Pre-existing Medical Conditions",
+      "Extreme Adventure Activities",
+    ],
+  },
+
+  {
+    id: "17",
+    logo: "https://via.placeholder.com/150/33A5FF/FFFFFF?text=Company+G",
+    company: "PetCare Insurance",
+    name: "PetSecure Plus",
+    coverage: "Veterinary Expenses, Accident Coverage, Annual Checkups",
+    premium: "$40 per month",
+    description:
+      "Comprehensive pet insurance covering veterinary expenses, accident coverage, and annual checkups for dogs and cats.",
+    additionalBenefits: [
+      "Boarding Kennel Fees Coverage",
+      "Lost Pet Recovery",
+      "Prescription Medications for Pets",
+    ],
+    exclusions: ["Pre-existing Conditions", "Cosmetic Procedures for Pets"],
+  },
+
+  {
+    id: "18",
+    logo: "https://via.placeholder.com/150/33A5FF/FFFFFF?text=Company+G",
+    company: "HomeGuard Insurance",
+    name: "Guardian Home Insurance",
+    coverage: "Property Damage, Theft, Personal Liability",
+    premium: "$800 per year",
+    description:
+      "Home insurance protecting against property damage, theft, and personal liability, ensuring comprehensive coverage for homeowners.",
+    additionalBenefits: [
+      "Temporary Living Expenses Coverage",
+      "Identity Theft Protection",
+      "Valuable Items Coverage",
+    ],
+    exclusions: ["Flood Damage", "Acts of Terrorism"],
+  },
+
+  {
+    id: "19",
+    logo: "https://via.placeholder.com/150/33A5FF/FFFFFF?text=Company+G",
+    company: "BusinessShield Assurance",
+    name: "BusinessSecure Plus",
+    coverage: "Business Property, Liability Insurance, Business Interruption",
+    premium: "$200 per month",
+    description:
+      "Tailored insurance for businesses, providing coverage for business property, liability, and protection against interruptions and losses.",
+    additionalBenefits: [
+      "Professional Liability Coverage",
+      "Cybersecurity Insurance",
+      "Employee Benefits Coverage",
+    ],
+    exclusions: [
+      "Acts of Fraud by Business Owners",
+      "Unlawful Business Activities",
+    ],
+  },
+
+  {
+    id: "20",
+    logo: "https://via.placeholder.com/150/33A5FF/FFFFFF?text=Company+G",
+    company: "FutureGuard Education Insurance",
+    name: "CollegePlus Education Plan",
+    coverage: "Tuition Fees, Education Savings, Student Loan Protection",
+    premium: "$100 per month",
+    description:
+      "Education insurance designed to cover tuition fees, build education savings, and protect against student loan burdens for future education expenses.",
+    additionalBenefits: [
+      "Scholarship Opportunities",
+      "Career Counseling Services",
+      "Flexible Premium Payment Plans",
+    ],
+    exclusions: [
+      "Non-Qualified Education Expenses",
+      "Educational Institutions Outside Designated Networks",
+    ],
+  },
+
+  // ... (more policies)
+];
+
+export default companyPolicies;
---
+++ I.POL-frontend/data/policy.js
@@ -0,0 +1,61 @@
+// policies.js
+
+const policies = [
+  {
+    id: "1",
+    logo: "https://via.placeholder.com/150/33A5FF/FFFFFF?text=Company+G",
+    name: "Basic Health Insurance",
+    coverage: "Hospitalization, Doctor Visits, Medications",
+    premium: "$50 per month",
+    description:
+      "Covers basic health expenses including hospitalization, doctor visits, and medications.",
+  },
+  {
+    id: "2",
+    logo: "https://via.placeholder.com/150/33A5FF/FFFFFF?text=Company+G",
+    name: "Auto Insurance",
+    coverage: "Collision, Liability, Comprehensive",
+    premium: "$75 per month",
+    description:
+      "Provides coverage for your vehicle against collisions, liability claims, and comprehensive damages.",
+  },
+  {
+    id: "3",
+    logo: "https://via.placeholder.com/150/33A5FF/FFFFFF?text=Company+G",
+    name: "Home Insurance",
+    coverage: "Property Damage, Theft, Personal Liability",
+    premium: "$100 per month",
+    description:
+      "Protects your home against property damage, theft, and provides personal liability coverage.",
+  },
+  {
+    id: "4",
+    logo: "https://via.placeholder.com/150/33A5FF/FFFFFF?text=Company+G",
+    name: "Comprehensive Health Plan",
+    coverage: "Hospitalization, Doctor Visits, Medications",
+    premium: "$50 per month",
+    description:
+      "Covers basic health expenses including hospitalization, doctor visits, and medications.",
+  },
+  {
+    id: "5",
+    logo: "https://via.placeholder.com/150/33A5FF/FFFFFF?text=Company+G",
+    name: "Auto Protection Plus",
+    coverage: "Collision, Liability, Comprehensive",
+    premium: "$75 per month",
+    description:
+      "Provides coverage for your vehicle against collisions, liability claims, and comprehensive damages.",
+  },
+  {
+    id: "6",
+    logo: "https://via.placeholder.com/150/33A5FF/FFFFFF?text=Company+G",
+    name: "Premier Home Coverage",
+    coverage: "Property Damage, Theft, Personal Liability",
+    premium: "$100 per month",
+    description:
+      "Protects your home against property damage, theft, and provides personal liability coverage.",
+  },
+  // Add more policy objects as needed
+];
+
+export default policies;
